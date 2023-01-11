aa4b3d99927a8ccc7fa8975f7133af59f32eeb5e
Herykurniawann <herykurniawansartono@gmail.com>
2023-01-11 09:41:53 +0700

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["[public/**/*.{html,js}"],
+  content: [
+    "./public/**/*{html,js}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#123043',
+      },
+      fontFamily: {
+        "quick-sand": ["Quicksand", "sans-serif"],
+      },
+      screens: {
+        'phone': '414px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
+
